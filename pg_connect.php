235359d13f156e542ab14886df648c2987005216
Zilitech <zilitechy@gmail.com>
2025-11-03 16:17:46 +0530

    pg_connect deleted

--- pg_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = pg_connect("host=82.112.226.186 port=5432 dbname=mydb user=myuser password=mypassword");
-
-if ($conn) {
-    echo "✅ Connected to PostgreSQL successfully!";
-} else {
-    echo "❌ Connection failed: " . pg_last_error();
-}
-?>
